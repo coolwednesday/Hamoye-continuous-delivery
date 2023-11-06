ab91a0fc9f798246a3db410e5ccd2df39918e619
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2023-11-06 18:15:39 +0530

    Update create-workflow-pr.sh

--- .github/script/create-workflow-pr.sh
+++ .github/script/create-workflow-pr.sh
@@ -7,11 +7,21 @@ git config user.email github-actions@github.com
 
 # If --pull-first is set, pull latest from main
 # before creating pull request
+
+
+# Create an empty commit
+git commit --allow-empty -m "$COMMIT_MESSAGE"
+
+# Push the branch to the remote repository
+git push origin $BRANCH_NAME
+
+# Optionally, switch back to the previous branch (e.g., "main")
+git checkout main
 if [ "$1" = "--pull-first" ]
 then
     echo "Merging main into $PR_BRANCH"
-    git checkout $PR_BRANCH
-    git pull origin main --no-rebase -X theirs --no-edit
+    git checkout -b $PR_BRANCH
+    git pull origin main 
     git push origin $PR_BRANCH
 fi
 
