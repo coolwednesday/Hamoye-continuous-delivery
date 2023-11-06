fa31d6591e07b5486d34452ffa53b18bb56be0c1
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2023-11-06 18:26:44 +0530

    Update create-workflow-pr.sh

--- .github/script/create-workflow-pr.sh
+++ .github/script/create-workflow-pr.sh
@@ -5,18 +5,21 @@
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-
-
+# Define your branch name, commit message, and other variables
+PR_BRANCH="azure-configuration"
+COMMIT_MESSAGE="Initial empty commit for Azure configuration"
+PR_TITLE="Your pull request title"
 
 if [ "$1" = "--pull-first" ]
 then
     echo "Merging main into $PR_BRANCH"
     git checkout -b $PR_BRANCH
-    git pull origin main 
-    # Create an empty commit
-    git commit --allow-empty -m "$COMMIT_MESSAGE"
-    git push origin $PR_BRANCH
+    git pull origin main
 fi
+
+# Create an empty commit
+git commit --allow-empty -m "$COMMIT_MESSAGE"
+git push origin $PR_BRANCH
 
 echo "Create pull request for $PR_BRANCH into main"
 gh pr create --base main --head $PR_BRANCH --title "$PR_TITLE" --body ""
